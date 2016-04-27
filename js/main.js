85ab1e66bdbf970d10db2397cd03524f2260eeb1
Edoardo Tenani <edoardo.tenani@gmail.com>
2016-04-27 22:27:48 +0200

    first mvp deployed

---
+++ js/main.js
@@ -0,0 +1,140 @@
+function getResponseFromServer(data_to_send) {
+    $.ajax({
+	type: 'CHESS',
+	url: window.location.href+data_to_send,
+	timeout: 15000,
+	success: function(data) {
+		data=data.split("#");
+		board.position(data[0]);
+		actual_turn=data[1]
+		whoami=data[2]
+	},
+	error: function(XMLHttpRequest, textStatus, errorThrown) {}
+	})
+}
+
+
+var board
+var whoami="w"
+var actual_turn="w"
+
+var removeGreySquares = function() {
+  $('#board .square-55d63').css('background', '');
+};
+
+var greySquare = function(square) {
+  var squareEl = $('#board .square-' + square);
+  
+  var background = '#a9a9a9';
+  if (squareEl.hasClass('black-3c85d') === true) {
+    background = '#696969';
+  }
+
+  if (actual_turn===whoami){
+    background = '#b0c4de';
+    if (squareEl.hasClass('black-3c85d') === true) {
+      background = '#6495ed';
+    }
+  }
+
+  squareEl.css('background', background);
+};
+
+var onDragStart = function(source, piece) {
+  // do not pick up pieces if the game is over
+  // or if it's not that side's turn
+  if (piece==="fog"){
+    return false;
+  }
+  if (actual_turn === "gameover" ||
+      (whoami === 'w' && piece.search(/^b/) !== -1) ||
+      (whoami === 'b' && piece.search(/^w/) !== -1)) {
+    return false;
+  }
+};
+
+var onDrop = function(source, target) {
+  response = getResponseFromServer(source+"."+target);
+  
+  removeGreySquares();  
+
+};
+
+var onMouseoverSquare = function(square, piece) {
+/*
+  // get list of possible moves for this square
+  var moves = game.moves({
+    square: square,
+    verbose: true
+  });
+*/
+moves=0;
+  // exit if there are no moves available for this square
+  if (moves.length === 0) return;
+
+  // highlight the square they moused over
+  greySquare(square);
+
+  // highlight the possible squares for this piece
+  for (var i = 0; i < moves.length; i++) {
+    greySquare(moves[i].to);
+  }
+};
+
+var onMouseoutSquare = function(square, piece) {
+  removeGreySquares();
+};
+
+var onSnapEnd = function() {
+};
+
+var cfg = {
+  draggable: true,
+  position: "",
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onMouseoutSquare: onMouseoutSquare,
+  onMouseoverSquare: onMouseoverSquare,
+  onSnapEnd: onSnapEnd
+};
+board = ChessBoard('board', cfg);
+
+window.setInterval(function(){
+  $.ajax({
+  type: 'CHESS',
+  url: window.location.href+"update",
+  timeout: 15000,
+  success: function(data) {
+    data=data.split("#");
+    board.position(data[0]);
+    actual_turn=data[1]
+    whoami=data[2]
+    if (whoami==="w"){
+      board.orientation("white");
+    }
+    else if (whoami==="b"){
+      board.orientation("black");
+    }
+    
+    if (actual_turn===whoami){
+      $('#turno').html("E' il tuo turno!");
+    }
+    else{
+      $('#turno').html("E' il turno avversario..."); 
+    }
+    mangiati=[]
+    for (var temp in data[3]){
+      if (data[3][temp]==data[3][temp].toUpperCase()){
+        mangiati.push("w"+data[3][temp])
+      }
+      else{
+       mangiati.push("b"+data[3][temp].toUpperCase()) 
+      }
+    }
+    
+    ChessBoard.mangiati(mangiati)
+  },
+  error: function(XMLHttpRequest, textStatus, errorThrown) {}
+  })
+}, 1000);
+
---
+++ server.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python
+import random
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+import chess
+import time
+import urlparse
+
+PORT_NUMBER = 80
+
+games = {}
+player_ids = []
+
+def generate_new_game():
+    actual_name = 'a'
+    while actual_name in games.keys():
+        last_letter = ord(actual_name[-1])+1
+        if last_letter > 122:
+            actual_name[-1] ='a'
+            last_letter = 97
+        else:
+            actual_name = actual_name[:-1]
+        actual_name += chr(last_letter)
+
+    games[actual_name] = [chess.Game(),None,None]
+
+    return actual_name
+
+def generate_player_id():
+    actual_name = 'aaa'
+    while actual_name in player_ids:
+        last_letter = ord(actual_name[-1])+1
+        if last_letter > 122:
+            actual_name[-1] ='a'
+            last_letter = 97
+        else:
+            actual_name = actual_name[:-1]
+        actual_name += chr(last_letter)
+
+    player_ids.append(actual_name)
+
+    return actual_name
+
+def player(game, address):
+    if game[1]==address:
+        return "w"
+    elif game[2]==address:
+        return "b"
+    else:
+        return "none"
+
+class myHandler(BaseHTTPRequestHandler):
+    def do_CHESS(self):
+        global games
+        if self.path[-7:]=="/update":
+            game_id = self.path.split('/')[0] # /a/update
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+            if not game_id in games.keys():
+                self.wfile.write("game not found")
+                return
+
+            game = games[game_id]
+            
+            p = player(game,self.client_address[0])
+            if not p=="none":
+                self.wfile.write(game.to_fen(p))
+            elif self.client_address[0]==player_white:
+                self.wfile.write(game.to_fen("w"))
+            elif self.client_address[0]==player_black:
+                self.wfile.write(game.to_fen("b"))
+            elif player_white==None:
+                player_white=self.client_address[0]
+                print "white is "+str(self.client_address[0])
+                self.wfile.write(game.to_fen("w"))
+            elif player_black==None:
+                player_black=self.client_address[0]
+                print "black is "+str(self.client_address[0])
+                self.wfile.write(game.to_fen("b"))
+            else:
+                self.wfile.write("the game is full")
+        else:
+            if game.end():
+                game=chess.Game()
+            data = self.path[1:].split(".")
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            p = player(self.client_address[0])
+            game.move(data[0],data[1])
+            self.wfile.write(game.to_fen(p))
+
+    def do_GET(self):
+        if self.path[0:6] == "/game/":
+            url = urlparse.urlparse(self.path)
+            url_params = urlparse.parse_qs(url.query)
+            
+            player_id = url_params["player_id"]
+            game_id = url.path[6:]
+
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+
+            if game_id in games.keys():
+                if games[game_id][1]==None:
+                    games[game_id][1]=player_id
+                elif games[game_id][2]==None:
+                    games[game_id][2]=player_id
+                else:
+                    return self.wfile.write('sorry, the game is full')    
+                in_file=open("index.html","r")
+                data = in_file.read()
+                in_file.close()
+                # Send the html message
+                self.wfile.write(data)
+            else:
+                self.wfile.write('game not found')
+        elif self.path[0:5] == '/join':
+            url = urlparse.urlparse(self.path)
+            game_id = url.path[6:]
+
+            # generata hash for new user
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            player_id = generate_player_id()
+            self.wfile.write('Joined game. <a href="/game/'+game_id+'?player_id='+player_id+'">Go to game</a>')
+        elif self.path == '/new':
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            # New game logic!
+            new_game_hash=generate_new_game()
+            games[new_game_hash][1]=self.client_address[0]
+            # self.wfile.write('')
+            player_id = generate_player_id()
+            self.wfile.write('New game created. <a href="/game/'+new_game_hash+'?player_id='+player_id+'">Go to game</a>')
+        else:
+            test=self.path.split(".")[-1]
+            try:
+                self.send_response(200)
+                if test=="css":
+                    self.send_header('Content-type','text/css')
+                elif test=="png":
+                    self.send_header('Content-type','image/png')
+                else:
+                    self.send_header('Content-type','text/html')
+                
+                self.end_headers()
+                if test=="png":
+                    in_file=open(self.path[1:],"rb")
+                else:
+                    in_file=open(self.path[1:],"r")
+                data = in_file.read()
+                in_file.close()
+                # Send the html message
+                self.wfile.write(data)
+            except:
+                self.wfile.write("file not found")
+
+
+try:
+    #Create a web server and define the handler to manage the
+    #incoming request
+    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    print 'Started httpserver on port ' , PORT_NUMBER
+    
+    #Wait forever for incoming htto requests
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    print '^C received, shutting down the web server'
+    server.socket.close()
